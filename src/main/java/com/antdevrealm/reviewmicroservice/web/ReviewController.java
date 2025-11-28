a6c3741373442eeac194283de9ef4f92dd6cc177
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-11-28 13:27:42 +0100

    fix(review creation/web) - add missing @RequestBody annotation

--- src/main/java/com/antdevrealm/reviewmicroservice/web/ReviewController.java
+++ src/main/java/com/antdevrealm/reviewmicroservice/web/ReviewController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,7 @@ public class ReviewController {
     }
 
     @PostMapping
-    public ResponseEntity<ReviewResponseDTO> create(@Valid CreateReviewRequestDTO dto) {
+    public ResponseEntity<ReviewResponseDTO> create(@RequestBody @Valid CreateReviewRequestDTO dto) {
         ReviewResponseDTO responseDTO = this.reviewService.create(dto);
 
         URI uriLocation = URI.create("/api/v1/reviews/" + responseDTO.id());
