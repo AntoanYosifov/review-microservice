8739eb59ca131304d377d065dd8e7c8ae4e9fc5e
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-11-28 12:58:22 +0100

    feat(review) - add ReviewService

---
+++ src/main/java/com/antdevrealm/reviewmicroservice/service/ReviewService.java
@@ -0,0 +1,15 @@
+package com.antdevrealm.reviewmicroservice.service;
+
+import com.antdevrealm.reviewmicroservice.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    private final ReviewRepository reviewRepository;
+
+    @Autowired
+    public ReviewService(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+}
